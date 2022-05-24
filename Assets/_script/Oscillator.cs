1ea1ce166549dce3a167ae900b51fd692adab478
Nebularswamp <grosswamp@gmail.com>
2022-05-24 18:25:15 -0400

    Basic music scale notes generator

--- Assets/_script/Oscillator.cs
+++ Assets/_script/Oscillator.cs
@@ -16,35 +16,107 @@ public class Oscillator : MonoBehaviour
     public float gain;  //for "input"
     public float volume = 0.1f; //for "output"
 
-    public float[] frequencies;
+    public float[] frequencies; //stores musical Notes!
     public int freqIndex;
 
     public float tempo = 5f;
     public float sequenceTime = 1f;
     public float timeNow = 1f;
 
+
+    //holds the notes in a musical scale
+    [SerializeField] private float[] scaleNotes;
+
     private void Start()
     {
+
+        InitializeFrequencies();
+        FindScale(0); //C by default
     }
 
+    //assign numbers to notes
+    // 2 octaves of 12 equal temperament
+    private void InitializeFrequencies()
+    {
+        frequencies = new float[24];
+        //C4-B4
+        frequencies[0] = 262; //C4
+        frequencies[1] = 277; //CS4
+        frequencies[2] = 294; //D4
+        frequencies[3] = 311; //DS4
+        frequencies[4] = 330; //E4
+        frequencies[5] = 349; //F4
+        frequencies[6] = 370; //FS4
+        frequencies[7] = 392; //G4
+        frequencies[8] = 415; //GS4
+        frequencies[9] = 440; //A4
+        frequencies[10] = 466; //AS4
+        frequencies[11] = 494; //B4
 
+        //C5-B5
+        frequencies[12] = 523; //C5
+        frequencies[13] = 554; //CS5
+        frequencies[14] = 587; //D5
+        frequencies[15] = 622; //DS5
+        frequencies[16] = 659; //E5
+        frequencies[17] = 698; //F5
+        frequencies[18] = 740; //FS5
+        frequencies[19] = 784; //G5
+        frequencies[20] = 831; //GS5
+        frequencies[21] = 880; //A5
+        frequencies[22] = 932; //AS5
+        frequencies[23] = 988; //B5
+
+        gain = volume;
+    }
+
+    
     private void Update()
     {
+
+        
         //basic beat 
         timeNow -= tempo * Time.deltaTime;
         if (timeNow <= 0)
         {
-            timeNow = sequenceTime;
-            if(gain == volume)
+            frequency = scaleNotes[freqIndex];
+            freqIndex++;
+
+            if (freqIndex >= scaleNotes.Length - 1)
             {
-                gain = 0;
-            }else if(gain != volume)
-            {
-                gain = volume;
+                freqIndex = 0;
             }
+
+            timeNow = sequenceTime;
+
         }
 
+    }
 
+    private void Beat()
+    {
+        //intermittent
+        if (timeNow <= 0)
+        {
+
+            timeNow = sequenceTime;
+            if (gain == volume)
+            {
+                gain = 0;
+            }
+            else if (gain != volume)
+            {
+                gain = volume;
+
+                frequency = scaleNotes[freqIndex];
+                freqIndex++;
+
+                if (freqIndex >= scaleNotes.Length - 1)
+                {
+                    freqIndex = 0;
+                }
+            }
+        }
     }
 
     private void BasicInput()
@@ -61,9 +133,16 @@ public class Oscillator : MonoBehaviour
     }
 
     // select the notes in the musical scale by formula
-    public void FindScale()
+    public void FindScale(int offset) //offset is for index of starting note (C4 =0, E4= 6, etc)
     {
+        //assuming the scale has 8 notes
+        //otherwise pentatone (5) or blues scale(7)
+        scaleNotes = new float[9];
 
+        for(int i = 0; i < scaleNotes.Length; i++)
+        {
+            scaleNotes[i] = frequencies[i * 2 + offset]; //Major scale
+        }
     }
 
     //this make sound!
