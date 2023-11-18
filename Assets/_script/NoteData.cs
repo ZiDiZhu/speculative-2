7b2ba2ba255eecb9c91c077a995cedf350ef13a0
Zi Di <grosswamp@gmail.com>
2023-11-17 20:10:18 -0500

    Added Metronome

---
+++ Assets/_script/NoteData.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Key{
+    C,
+    D,
+    E,
+    F,
+    G,
+    A,
+    B
+}
+
+
+public class NoteData : ScriptableObject
+{
+    public Key key;
+    public int octave;
+    public float frequency;
+}
