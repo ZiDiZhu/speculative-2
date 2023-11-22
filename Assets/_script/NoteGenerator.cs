6e7614592db59f361b4daad1590115adb7da94ad
Zi Di <grosswamp@gmail.com>
2023-11-21 23:30:50 -0500

    More Accurate Note Generation

--- Assets/_script/NoteGenerator.cs
+++ Assets/_script/NoteGenerator.cs
@@ -7,7 +7,7 @@ public class NoteGenerator : MonoBehaviour
     public int numOctaves = 8; // Number of octaves to generate
     public float duration = 1.0f;
     //public AudioClip sound;
-    public float baseFrequency = 440.0f; // Frequency of A4 (440 Hz)
+    public float baseFrequency = 16.35f; // Frequency of C0
 
     public NoteData noteDataScriptableObj;
 
@@ -21,7 +21,7 @@ public class NoteGenerator : MonoBehaviour
             // Create the folder if it doesn't exist
             AssetDatabase.CreateFolder("Assets", "NoteData");
         }
-        for (int octave = 1; octave <= numOctaves; octave++)
+        for (int octave = 0; octave <= numOctaves; octave++)
         {
             for (int noteIndex = 0; noteIndex < 12; noteIndex++) // 12 notes in an octave
             {
@@ -29,15 +29,17 @@ public class NoteGenerator : MonoBehaviour
                 NoteData noteData = Instantiate(noteDataScriptableObj);
 
                 // Generate note name based on note index and octave
-                noteData.name = noteNamePrefix + (noteIndex + 1) + "-" + octave;
+                noteData.name = GetNoteName(noteIndex, octave);
 
                 // Calculate the frequency based on the note's position relative to A4
                 int semitoneOffset = GetSemitoneOffset(noteIndex);
-                noteData.frequency = baseFrequency * Mathf.Pow(2.0f, (float)semitoneOffset / 12.0f);
+                noteData.frequency = baseFrequency * Mathf.Pow(2.0f, (float)(semitoneOffset+(octave * 12) )/ 12.0f);
+                noteData.octave = octave;
 
                 // Assign the attributes from the Inspector
                 noteData.duration = duration;
                 //noteData.sound = sound;
+                
 
                 // Save the NoteData asset
                 string assetPath = "Assets/NoteData/" + noteData.name + ".asset";
@@ -57,5 +59,12 @@ public class NoteGenerator : MonoBehaviour
 
         // Calculate the semitone offset based on the note index
         return semitoneOffsets[noteIndex % 7];
+    }
+
+    // Helper method to generate note name with octave prefix
+    private string GetNoteName(int noteIndex, int octave)
+    {
+        string[] noteNames = { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
+        return noteNames[noteIndex] + (octave + 1); // Octave number starts from 1
     }
 }
