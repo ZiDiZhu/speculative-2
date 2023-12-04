85e11f1107cd4b216fd0bf70c9ceaf0f066029db
Zi Di <grosswamp@gmail.com>
2023-12-04 18:36:38 -0500

    Folder Organization2

--- Assets/_Script/common_scripts/OnLoadEvent.cs
+++ Assets/_Script/common_scripts/OnLoadEvent.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class OnLoadEvent : MonoBehaviour //triggers 1 event on load after a time
+public class OnLoad : MonoBehaviour //triggers 1 event on load after a time
 {
     public float waitSeconds;
     public UnityEvent startEvent;
